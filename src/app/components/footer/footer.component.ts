737aa8340331752770475d2d795f725a2ae4876e
Rubino <rossella.rubino@accenture.com>
2024-03-06 12:00:16 +0100

    feat:custom footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+
+    email: string = 'help.cse@regione.toscana.it@pec.gov.it';
+    phoneNumber: string = ' 800 004 477';
+    time: string = 'lun-ven 9:00 - 19:00, sab 9:00 - 13:00';
+}
