b0879b2a33fa57526c7af72e1cc88bb0a2da2594
Rossella Rubino <rossella.rubino@accenture.com>
2024-03-18 15:35:51 +0100

    fix: modal changes

--- src/app/components/modals/modal-profile/modal-profile.component.ts
+++ src/app/components/modals/modal-profile/modal-profile.component.ts
@@ -1,15 +1,14 @@
 import { AfterViewInit, Component, ViewChild, Input} from '@angular/core';
 import { ItModalComponent } from 'design-angular-kit';
-
  
 @Component({
   selector: 'app-modal-profile',
   templateUrl: './modal-profile.component.html',
   styleUrl: './modal-profile.component.scss'
 })
+
 export class ModalProfileComponent implements AfterViewInit {
   @ViewChild('modalProfile') modalProfile!: ItModalComponent;
-  
 
   @Input() profileDetail: any;
   
@@ -20,10 +19,15 @@ export class ModalProfileComponent implements AfterViewInit {
   firstModalVisible = true;
   secondModalVisible = false;
 
-  
+  userInput: string = '';
+  confirmInput: string = '';
+
   ngAfterViewInit(): void {
     // Rimuovo questa riga, poiché non voglio aprire automaticamente la modale all'inizio
     // this.modalProfile.toggle();
+
+    document.getElementById('newInput')!.addEventListener('input', () => this.setUserInputDisplay());
+    document.getElementById('confirmInput')!.addEventListener('input', () => this.setUserInputDisplay());
   }
 
   openModal(type: 'email' | 'phone'): void {
@@ -41,14 +45,23 @@ export class ModalProfileComponent implements AfterViewInit {
     this.modalProfile.toggle();
   }
 
+  setUserInputDisplay(): void {
+    const displayValue = this.isEmailType ? this.userInput : this.profileDetail?.phone;
+    document.getElementById('userInputDisplay')!.innerText = displayValue || '';
+  } 
+  
   showFirstModal() {
     this.firstModalVisible = true;
     this.secondModalVisible = false;
+    this.userInput = ''; 
+    this.confirmInput = ''; 
+    this.setUserInputDisplay();
   }
 
   showSecondModal() {
     this.firstModalVisible = false;
     this.secondModalVisible = true;
+    this.setUserInputDisplay();
   }
   
   // converto il tempo rimanente in formato mm:ss
@@ -57,5 +70,4 @@ export class ModalProfileComponent implements AfterViewInit {
     const seconds: number = this.remainingTime % 60;
     return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   }
-
 }
