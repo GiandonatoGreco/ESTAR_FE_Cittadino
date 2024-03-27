ff2e1ee5a7c20f1e4b508c217cc61cee8645e72f
Rossella Rubino <rossella.rubino@accenture.com>
2024-03-27 18:06:37 +0100

    feat: modify mobile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ProfileService } from 'services/profile.service';
 import { ProfileI } from 'models/profile';
 import { ModalProfileComponent } from 'components/modals/modal-profile/modal-profile.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-profile',
@@ -21,6 +22,8 @@ export class ProfileComponent implements OnInit {
   isEmailType: boolean = false;
   isCellulareType: boolean = false;
 
+  isMobile: boolean = false;
+
   crumbs: BreadcrumbI[] = [
     {
       link: '/' + routes.dashboard.path,
@@ -34,12 +37,19 @@ export class ProfileComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private profileService: ProfileService
+    private profileService: ProfileService,
+    private breakpointObserver: BreakpointObserver
   ) {} 
   ngOnInit(): void {
     this.profileService.getProfileDetails().subscribe((details) => {
       this.profileDetail = details;
     });
+
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isMobile = result.matches;
+      });
   }
 
   openModal(type: 'email' | 'phone'): void {
